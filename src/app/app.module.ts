df7d896c43d6e11d458d3e208acc414023783e9e
Jan G. Wieners <jan@jan-wieners.de>
2019-06-13 23:54:23 +0200

    Update dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import {AppComponent} from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {OralExaminationProtocolComponent} from './components/oral-examination-protocol/oral-examination-protocol.component';
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
